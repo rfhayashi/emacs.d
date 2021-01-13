d883018111b9a515521e02c453d06955aae0954a
Rui Fernando Hayashi <rfhayashi@gmail.com>
2021-01-13 10:32:20 -0300

    tangle and load config

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 (straight-use-package 'org)
 
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(require 'org)
+(org-babel-tangle-file (expand-file-name "config.org" user-emacs-directory))
+
+(load-file (expand-file-name "config.el" user-emacs-directory))
 
 ; make scratch buffer be the first
 (setq initial-buffer-choice t)
