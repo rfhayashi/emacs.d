97d0d6f9f59bc132925b7f27e10b7d8e5c28fb0d
Rui Fernando Hayashi <rfhayashi@gmail.com>
2021-02-28 19:00:01 -0300

    allow local nix packages

--- init.el
+++ init.el
@@ -5,8 +5,10 @@
 
 (require 'org)
 (org-babel-tangle-file (expand-file-name "config.org" user-emacs-directory))
-
 (load-file (expand-file-name "config.el" user-emacs-directory))
+(when (file-exists-p "~/.config/emacs/local.org")
+  (org-babel-tangle-file "~/.config/emacs/local.org")
+  (load-file "~/.config/emacs/local.el"))
 
 ; make scratch buffer be the first
 (setq initial-buffer-choice t)
