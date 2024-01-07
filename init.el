b89a8130be737522fcd095f47b12e7f1083c77c1
Rui Fernando Hayashi <rfhayashi@gmail.com>
2024-01-07 14:58:27 -0300

    window key bindings

--- init.el
+++ init.el
@@ -35,6 +35,11 @@
   :config
   (which-key-mode))
 
+(use-package winum
+  :config
+  (require 'winum)
+  (winum-mode))
+
 (use-package
   zenburn-theme
   :config
@@ -94,7 +99,21 @@
      ("k" . ("paredit"
 	     (("d" . paredit-kill)
 	      ("r" . paredit-raise-sexp)
-	      ("s" . paredit-forward-slurp-sexp))))))))
+	      ("s" . paredit-forward-slurp-sexp))))
+     ("w" . ("window"
+	     (("/" . split-window-right)
+	      ("-" . split-window-below)
+	      ("d" . delete-window))))
+     ("1" . winum-select-window-1)
+     ("2" . winum-select-window-2)
+     ("3" . winum-select-window-3)
+     ("4" . winum-select-window-4)
+     ("5" . winum-select-window-5)
+     ("6" . winum-select-window-6)
+     ("7" . winum-select-window-7)
+     ("8" . winum-select-window-8)
+     ("9" . winum-select-window-9)
+     ("0" . winum-select-window-0-or-10)))))
 
 (defun my-set-emacs-lisp-keys ()
   (evil-local-set-key
