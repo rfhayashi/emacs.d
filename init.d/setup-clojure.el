96f27b804faaebcaebba32329efae13ec4d3bca7
Rui Fernando Hayashi <rfhayashi@gmail.com>
2024-01-09 16:54:02 -0300

    cider setup

--- init.d/setup-clojure.el
+++ init.d/setup-clojure.el
@@ -1,3 +1,44 @@
 (use-package clojure-mode
   :config
   (add-hook 'clojure-mode-hook 'enable-paredit-mode))
+
+(defun my-cider-test-run-focused-test ()
+  "Run test around point"
+  (interactive)
+  (cider-load-buffer)
+  (cider-test-run-test))
+
+(defun my-cider-test-run-ns-tests ()
+  "Run namespace test"
+  (interactive)
+  (cider-load-buffer)
+  (cider-test-run-ns-tests nil))
+
+(defun my-set-clojure-keys ()
+  (evil-local-set-key
+   'normal
+   (kbd ",")
+   (my-gen-keymap
+    '("local"
+      (("e" . ("eval"
+	       (("b" . cider-eval-buffer)
+		("e" . cider-eval-last-sexp)
+		("f" . cider-eval-defun-at-point)
+		("p" . cider-eval-print-last-sexp))))
+       ("s" . ("repl"
+	       (("q" . cider-quit)
+		("x" . cider-ns-refresh))))
+       ("t" . ("test"
+	       (("n" . my-cider-test-run-ns-tests)
+		("t" . my-cider-test-run-focused-test))))
+       ("'" . cider-jack-in))))))
+
+(use-package cider
+  :custom
+  (cider-save-file-on-load t)
+  (cider-repl-pop-to-buffer-on-connect nil)
+  (cider-test-defining-forms '("deftest" "defspec"))
+  (clojure-toplevel-inside-comment-form t)
+  (cider-clojure-cli-global-options "-J-XX:-OmitStackTraceInFastThrow")
+  :config
+  (add-hook 'clojure-mode-hook 'my-set-clojure-keys))
