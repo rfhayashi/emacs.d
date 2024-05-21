76e43c9590f7f8db84dfbf8cad500632c6322cca
Rui Fernando Hayashi <rfhayashi@gmail.com>
2024-05-21 10:08:15 -0300

    python support

---
+++ init.d/setup-python.el
@@ -0,0 +1,24 @@
+;;; -*- lexical-binding: t; -*-
+
+(use-package python
+  :custom
+  (python-shell-interpreter "ipython")
+  (python-shell-interpreter-args "--simple-prompt")
+  (python-shell-prompt-detect-failure-warning nil)
+  (python-indent-guess-indent-offset-verbose nil)
+  :config
+  (add-to-list 'python-shell-completion-native-disabled-interpreters "jupyter")
+  (add-hook 'inferior-python-mode-hook (lambda () (setq comint-move-point-for-output t))))
+
+(use-package conda
+  :custom
+  (conda-anaconda-home (expand-file-name "~/anaconda3"))
+  :config
+  (conda-env-initialize-interactive-shells)
+  (conda-env-initialize-eshell)
+  (conda-env-autoactivate-mode t)
+  (conda-env-activate "base"))
+
+(use-package elpy
+  :init
+  (elpy-enable))
