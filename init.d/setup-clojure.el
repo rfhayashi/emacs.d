8d8d3a5f5c65e0f12cba6d877fce3f6979e1a469
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-03-17 11:01:58 -0300

    run babashka tests

--- init.d/setup-clojure.el
+++ init.d/setup-clojure.el
@@ -5,12 +5,46 @@
   (clojure-mode . enable-paredit-mode)
   (clojure-mode . eglot-ensure))
 
+(defun my-cider-babashka-repl-p ()
+  (let ((repl-buf (cider-current-repl)))
+    (with-current-buffer repl-buf
+      (save-excursion
+	(goto-char (point-min))
+	(search-forward "Babashka" nil t)))))
+
+(let* ((template "
+(let [out (java.io.StringWriter.)
+      summary (binding [clojure.test/*test-out* out]
+                (clojure.test/run-test-var (var %s/%s)))]
+  {:summary summary
+   :out (str out)})
+")
+       (code (lambda (ns var)
+	       (format template ns var))))
+  (defun my-babashka-run-focused-test ()
+    (let* ((found (cider--extract-test-var-at-point))
+	   (found-ns (car found))
+	   (found-var (cadr found)))
+      (if (not found-var)
+	  (message "No test found at point")
+	(cider-nrepl-send-request
+	 `("op" "eval" "code" ,(funcall code found-ns found-var))
+	 (lambda (response)
+	   (when (nrepl-dict-contains response "value")
+	     (with-current-buffer (get-buffer-create "bb-test-report")
+	       (goto-char (point-max))
+	       (insert (gethash :out (parseedn-read-str (nrepl-dict-get response "value"))))
+	       (goto-char (point-max))))))))))
+
 (defun my-cider-test-run-focused-test ()
   "Run test around point"
   (interactive)
-  (cider-load-buffer)
-  (cider-test-run-test))
+  (cider-eval-defun-at-point) 
+  (if (my-cider-babashka-repl-p)
+      (my-babashka-run-focused-test)
+    (cider-test-run-test)))
 
+;; TODO support babashka
 (defun my-cider-test-run-ns-tests ()
   "Run namespace test"
   (interactive)
