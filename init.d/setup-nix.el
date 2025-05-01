39d59d45b5f9111a8e485b34441ffff0e68f648f
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-05-01 16:31:40 -0300

    enable nix-ts-mode

--- init.d/setup-nix.el
+++ init.d/setup-nix.el
@@ -1,8 +1,13 @@
 ;;; -*- lexical-binding: t; -*-
 
-(use-package nix-mode
+(use-package nix-mode)
+
+;; TODO associate
+(use-package nix-ts-mode
   :hook
-  (nix-mode . eglot-ensure))
+  (nix-mode . eglot-ensure)
+  :init
+  (add-to-list 'major-mode-remap-alist '(nix-mode . nix-ts-mode)))
 
 (defun my-nixos-switch ()
   (interactive)
