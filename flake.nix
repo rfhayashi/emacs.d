206aea9f21587148cd2dcd8f49428911338c04e1
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-05-01 14:40:42 -0300

    ensure git repo

--- flake.nix
+++ flake.nix
@@ -19,6 +19,19 @@
               programs.emacs.enable = true;
 
               home.file.".emacs.d/early-init.el".text = ''
+                (defun ensure-git-repo (dir repo-url)
+                  (let ((expanded-dir (expand-file-name dir)))
+                    (if (file-directory-p expanded-dir)
+                        (message "Directory %s already exists." expanded-dir)
+                      (let ((default-directory (file-name-directory expanded-dir)))
+                        (message "Cloning %s into %s..." repo-url expanded-dir)
+                        (let ((exit-code (call-process "git" nil "*git-clone-output*" t "clone" repo-url expanded-dir)))
+                          (if (zerop exit-code)
+                              (message "Cloned %s successfully." repo-url)
+                            (message "Failed to clone %s. See *git-clone-output* buffer for details." repo-url)))))))
+
+                (ensure-git-repo "${config.programs.emacs.userDir}" "git@github.com:rfhayashi/emacs.d")
+
                 (setq user-emacs-directory "${config.programs.emacs.userDir}")
 
                 (let ((early-init-file (expand-file-name "early-init.el" user-emacs-directory)))
