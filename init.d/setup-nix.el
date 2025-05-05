da10cd8422773acd9ee96ab278b881da10013280
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-05-05 12:19:06 -0300

    remove nix-mode

--- init.d/setup-nix.el
+++ init.d/setup-nix.el
@@ -1,13 +1,10 @@
 ;;; -*- lexical-binding: t; -*-
 
-(use-package nix-mode)
-
-;; TODO associate
 (use-package nix-ts-mode
   :hook
   (nix-mode . eglot-ensure)
   :init
-  (add-to-list 'major-mode-remap-alist '(nix-mode . nix-ts-mode)))
+  (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-ts-mode)))
 
 (defun my-nixos-switch ()
   (interactive)
