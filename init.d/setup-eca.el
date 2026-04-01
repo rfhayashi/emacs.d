adec73cdb20cfdfea607049d5c0d8703a24e3960
Rui Fernando Hayashi <rfhayashi@gmail.com>
2026-04-01 16:31:54 -0300

    eca key bindings

--- init.d/setup-eca.el
+++ init.d/setup-eca.el
@@ -1,3 +1,8 @@
 ;;; -*- lexical-binding: t; -*-
 
-(use-package eca)
+(use-package eca
+  :general
+  (space-key-map
+   "e" '(:ignore t :which-key "eca")
+   "em" 'eca-chat-select-model
+   "ea" 'eca-chat-select-agent))
