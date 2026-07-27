3e941172a0f789d9f2e811843312b71e9ff17203
Rui Fernando Hayashi <rfhayashi@gmail.com>
2026-07-27 17:02:04 -0300

    eca company mode

--- init.d/setup-eca.el
+++ init.d/setup-eca.el
@@ -1,8 +1,10 @@
 ;;; -*- lexical-binding: t; -*-
 
 (use-package eca
+  :hook (eca-chat-mode . company-mode)
   :general
   (space-key-map
    "e" '(:ignore t :which-key "eca")
+   "ee" 'eca
    "em" 'eca-chat-select-model
    "ea" 'eca-chat-select-agent))
